25213d9c143099fbd56cc4e16f8351f9f9ba793a
Hassan Dahmouchi <hassandahmouchi0@gmail.com>
2023-05-08 01:33:27 +0100

    hello

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -142,13 +142,6 @@
     </div>
   </section>
   <!-- End Contact Section -->
-<<<<<<< HEAD
-=======
-  
-  
-  <script src="{{asset('jsFolder/all.min.js')}}"></script>
-<script src="{{asset('jsFolder/master2.js')}}"></script>
-</body>
 
-</html>
->>>>>>> 683e812b614ab38b3e69cfe1a9bdd62093b7813a
+<script src="{{asset('jsFolder/all.min.js')}}"></script>
+<script src="{{asset('jsFolder/master2.js')}}"></script>
